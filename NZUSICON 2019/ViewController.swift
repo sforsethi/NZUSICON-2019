e034fa10d2e09ea22047e959bb99c03d70f83705
Raghav Sethi <rsraghavsethi@gmail.com>
2018-12-18 14:23:38 +0530

    Initial Commit

---
+++ NZUSICON 2019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NZUSICON 2019
+//
+//  Created by The Taste Affair on 18/12/18.
+//  Copyright © 2018 Raghav Sethi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
